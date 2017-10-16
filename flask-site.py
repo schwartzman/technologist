ba3b959940fcbac15fc3f0bab7e9e9008b3c1133
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2017-10-16 18:48:22 +0700

    Flask app

---
+++ flask-site.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+from flask import Flask
+from flask import Markup
+from flask import g
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    g.curr = 'index'
+    return render_template('index.html')
+
+
+@app.route('/sites/')
+def sites():
+    g.curr = 'sites'
+    con = sqlite3.connect('db.sqlite')
+    con.row_factory = sqlite3.Row
+    cur = con.execute('''
+        SELECT slug, title, ssl, link, source, blurb
+        FROM sites
+        WHERE display=1
+        ORDER BY sort
+    ''')
+    folios = cur.fetchall()
+    return render_template('sites.html', folios=folios)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    g.curr = 'err'
+    return render_template('base.html',
+                           e404=Markup('<img src="https://http.cat/404.jpg">')
+                           ), 404
