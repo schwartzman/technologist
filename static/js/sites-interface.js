5834adf49a703508309ab14f0a9c10830d46bb85
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-07-02 19:08:52 +0700

    Refactor JS & update build scripts

--- static/js/sites-interface.js
+++ static/js/sites-interface.js
@@ -1,15 +1,15 @@
-var a = {
+const a = {
 	time: 512
-};
-a.qtime = a.time / 4;
-a.fast = a.time / 16;
+}
+a.qtime = a.time / 4
+a.fast = a.time / 16
 
-var m = {
+const m = {
 	basebox: 184,
 	gutter: 10
-};
-m.bigbox = m.basebox * 3 + m.gutter * 2;
-m.fullimg = m.bigbox - m.gutter * 4;
+}
+m.bigbox = m.basebox * 3 + m.gutter * 2
+m.fullimg = m.bigbox - m.gutter * 4
 
 $('#boxes')
 	.show()
@@ -20,60 +20,60 @@ $('#boxes')
 		fitWidth: true,
 		transitionDuration: a.time/1000+'s',
 		stagger: a.fast
-});
+})
 
 function refresh() {
-	$('#boxes').masonry('layout');
+	$('#boxes').masonry('layout')
 }
 
-function open(target) {
-	// where target is some .box
-	closy = target.next();
-	obox = target.parent();
-	obox.width(m.bigbox).height(m.bigbox).css('z-index', 100);
-	target.animate({width: m.bigbox+'px', height: m.bigbox+'px'}, a.time, function() {
-		closy.css('display','inline').animate({opacity: 1},a.qtime);
-	}).addClass('open');
-	target.children('img.full').animate({opacity: 1, width: m.fullimg},  a.time);
-	target.children('img.thumb').animate({top: m.gutter+'px', left: m.gutter+'px', width: 0, opacity: 0}, a.qtime);
-	target.children('div.desc').animate({top: '435px', opacity: 1}, a.time);
-	refresh();
+function open($target) {
+	// where $target is some .box
+	const $closy = $target.next()
+	const $obox = $target.parent()
+	$obox.width(m.bigbox).height(m.bigbox).css('z-index', 100)
+	$target.animate({width: m.bigbox+'px', height: m.bigbox+'px'}, a.time, () => {
+		$closy.css('display','inline').animate({opacity: 1},a.qtime);
+	}).addClass('open')
+	$target.children('img.full').animate({opacity: 1, width: m.fullimg},  a.time)
+	$target.children('img.thumb').animate({top: m.gutter+'px', left: m.gutter+'px', width: 0, opacity: 0}, a.qtime)
+	$target.children('div.desc').animate({top: '435px', opacity: 1}, a.time)
+	refresh()
 }
 
 function close() {
 	// where target is some .box
-	target = $('.open');
-	if (target.length) {
-		closy = target.next();
-		obox = target.parent();
-		obox.width(m.basebox).height(m.basebox).css('z-index', 'auto');
-		closy.css({'display': 'none', 'opacity': 0});
-		target.animate({width: m.basebox+'px', height: m.basebox+'px'},  a.time, function() {
-			target.removeClass('open');
-		});
-		target.children('img.full').animate({opacity: 0, width: '160px'},  a.time);
-		target.children('img.thumb').animate({top: 0, left: 0, width: '160px', opacity: 1}, a.time);
-		target.children('div.desc').animate({top: '160px', opacity: 0}, a.time);
-		refresh();
+	const $target = $('.open');
+	if ($target.length) {
+		const $closy = $target.next();
+		const $obox = $target.parent();
+		$obox.width(m.basebox).height(m.basebox).css('z-index', 'auto')
+		$closy.css({'display': 'none', 'opacity': 0})
+		$target.animate({width: m.basebox+'px', height: m.basebox+'px'},  a.time, () => {
+			$target.removeClass('open')
+		})
+		$target.children('img.full').animate({opacity: 0, width: '160px'},  a.time)
+		$target.children('img.thumb').animate({top: 0, left: 0, width: '160px', opacity: 1}, a.time)
+		$target.children('div.desc').animate({top: '160px', opacity: 0}, a.time)
+		refresh()
 	}
 }
 
-$('.closy').click(function(){
-	close();
-});
+$('.closy').click(() => {
+	close()
+})
 
-$('.box').click(function(){
-	$this = $(this);
+$('.box').click(e => {
+	const $this = $(e.currentTarget)
 	if ( !$this.hasClass('open') ){
-		close();
-		open($this);
+		close()
+		open($this)
 	}
-});
+})
 
 /////////////////////
 // Keyboard Circus //
 /////////////////////
 
-$(document).on('keydown', null, 'esc', function(){
-	close();
-});
+$(document).on('keydown', null, 'esc', () => {
+	close()
+})
--- static/js/tools-interface.js
+++ static/js/tools-interface.js
@@ -3,42 +3,42 @@ function req(arr) {
 }
 
 function handler(e, req) {
-	e.preventDefault();
+	e.preventDefault()
 	$.ajax({
 		dataType: 'json',
 		url: req,
-		success: function(res) {
+		success: res => {
 			if ( $('.res').hasClass('slumber') ) {
-				for (var i in res) {
-					$label = $('<label/>').attr('for', i).text(i);
-					$input = $('<input>').attr('type', 'text').attr('id', i).prop('readonly', true);
-					$('.res').append($label, $input);
+				for (let i in res) {
+					const $label = $('<label/>').attr('for', i).text(i);
+					const $input = $('<input>').attr('type', 'text').attr('id', i).prop('readonly', true)
+					$('.res').append($label, $input)
 				}
-				$('.res').show().removeClass('slumber');
+				$('.res').show().removeClass('slumber')
 			}
-			for (var i in res) {
+			for (let i in res) {
 				$('#'+i).val(res[i])
 			}
 		}
 	})
 }
 
-$('form.dicer').submit(function(e) {
-	var length = $('input[type="number"]').val();
-	var flavor = $('input[type="radio"]:checked').val();
-	handler(e, req(['/tools/dicer', flavor, length]));
-});
+$('form.dicer').submit(e => {
+	const $length = $('input[type="number"]').val()
+	const $flavor = $('input[type="radio"]:checked').val()
+	handler(e, req(['/tools/dicer', $flavor, $length]))
+})
 
-$('form.hasher').submit(function(e) {
-	var victim = encodeURIComponent($('textarea').val().trim());
+$('form.hasher').submit(e => {
+	const victim = encodeURIComponent($('textarea').val().trim())
 	if (victim.length > 0) {
-		handler(e, req(['/tools/hasher', victim]));
+		handler(e, req(['/tools/hasher', victim]))
 	} else {
-		e.preventDefault();
-		$('.res').hide();
+		e.preventDefault()
+		$('.res').hide()
 	}
-});
+})
 
-$('.res').on('click', 'label, input, textarea', function(){
-	$(this).select();
-});
+$('.res').on('click', 'label, input, textarea', e => {
+	$(e.currentTarget).select()
+})
